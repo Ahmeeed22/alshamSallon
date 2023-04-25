9cc25ec0f4c9c33d82da8ee7d772c9a2d415cce6
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-04-25 09:58:04 +0400

    add transaction validation

--- modules/transactionAccount/joi/transactionAccounts.validation.js
+++ modules/transactionAccount/joi/transactionAccounts.validation.js
@@ -3,18 +3,14 @@ const Joi = require("joi");
 module.exports = {
     addHistoryTransactionsSchema:{
         body:Joi.object().required().keys({
-            details : Joi.string().min(3).required(),
             company_id : Joi.number().min(1).required() ,
-            transaction_id : Joi.number().min(1).required() ,
             amount : Joi.number().min(0).required()
         })
     },
     updateHistoryTransactionsSchema:{
         body:Joi.object().required().keys({
-            details : Joi.string().min(3).required(),
             company_id : Joi.number().min(1) ,
-            transaction_id : Joi.number().min(1) ,
-
+            amount : Joi.number().min(0).required()
         }).min(1)
     }
 }
